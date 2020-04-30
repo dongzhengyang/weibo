44baf303530931b53f175e099b26db2e05e2bc7a
vagrant <dongzhengyang@gmail.com>
2020-04-30 07:00:25 +0000

    创建会话

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,8 @@ Route::get('/signup', 'UsersController@create')->name('signup');
 Route::resource('users','UsersController');
 
 
+Route::get('login','SessionsController@create')->name('login');
+Route::Post('login','SessionsController@store')->name('login');
+Route::delete('logout','SessionsController@destory')->name('logout');
+
+
